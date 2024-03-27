d380892c2d58f49a1fdbb5709e45217a3e20fd55
Dinothan <dinothan1@gmail.com>
2024-03-27 11:45:16 +0530

    Task 1: Welcome screen added with alert message

---
+++ src/components/Layout.tsx
@@ -0,0 +1,38 @@
+import React, {ReactNode} from 'react';
+import {
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+  KeyboardAvoidingViewProps,
+} from 'react-native';
+
+interface LayoutProps {
+  children: ReactNode;
+  keyboardAvoidingViewProps?: KeyboardAvoidingViewProps;
+}
+
+const Layout: React.FC<LayoutProps> = ({
+  children,
+  keyboardAvoidingViewProps,
+}) => (
+  <View style={styles.background}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+      {...keyboardAvoidingViewProps}>
+      {children}
+    </KeyboardAvoidingView>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 25,
+  },
+});
+
+export default Layout;
---
+++ src/screens/Welcome.tsx
@@ -0,0 +1,52 @@
+import React, {FC, useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import AlertComponent from '../components/Alert';
+import Layout from '../components/Layout';
+
+const WelcomeScreen: FC = () => {
+  const [isAlertVisible, setIsAlertVisible] = useState<boolean>(false);
+
+  const showAlert = () => {
+    setIsAlertVisible(true);
+  };
+
+  const hideAlert = () => {
+    setIsAlertVisible(false);
+  };
+
+  return (
+    <Layout>
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.textStyle}>Welcome to </Text>
+          <Header>Stack Technologies</Header>
+        </View>
+
+        <Button mode="contained" onPress={showAlert}>
+          Login
+        </Button>
+
+        <AlertComponent
+          title="Stack Technologies"
+          description={"Please click 'Ok' to Login"}
+          visible={isAlertVisible}
+          hideAlert={hideAlert}
+        />
+      </View>
+    </Layout>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textContainer: {paddingBottom: 20},
+  textStyle: {textAlign: 'center', fontSize: 16, fontWeight: 'bold'},
+});
+
+export default WelcomeScreen;
